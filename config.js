a05b91df6a34c917372a5203dfa16023f3e2a14a
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-08-07 10:40:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ global.Owner = {
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
+  '6282142185506': { // put your number here
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
+  '6282142185506': { // put your number here
     name: 'Dave',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
